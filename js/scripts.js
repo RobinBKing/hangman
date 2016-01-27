11a407197d94e4fec4eaf034ecf828190ba9c41d
Jordan Meier and Rob King <jbirdmeier@gmail.com>
2016-01-27 09:56:50 -0800

    add letterArray prototype method

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,17 @@ function RandomWord (word) {
   this.word = word;
 }
 
+RandomWord.prototype.letterArray = function () {
+  return this.word.split('');
 
+}
 
-
-
-
-
-
+// RandomWord.prototype.letterCount = function () {
+//   return this.letterArray().length;
+// }
+//
+//
+//
 
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -3,8 +3,19 @@ describe('Words', function() {
     var testRandomWord = new RandomWord("Ocean");
     expect(testRandomWord.word).to.equal("Ocean");
   });
+
+  it("will add letterArray method to each RandomWord", function () {
+    var testRandomWord = new RandomWord("Ocean");
+    expect(testRandomWord.letterArray()).to.eql["O","c","e","a","n"];
+  });
+
+  // it("will add letterCount method to each RandomWord", function () {
+  //   var testRandomWord = new RandomWord("Ocean");
+  //   expect(testRandomWord.letterCount()).to.equal(5);
+  // });
+
   // it("will generate a list of words", function() {
-  //   var testWords = new Words([]);
-  //   expect(testWords.word).to.equal([]);
+  //   var testWords = new Words(["ocean", "beach", "waves", "palm tree", "coconut", "margarita"]);
+  //   expect(testWords.word).to.equal(["ocean", "beach", "waves", "palm tree", "coconut", "margarita"]);
   // });
 });
