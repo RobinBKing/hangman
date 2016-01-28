59296bbcf20ae5af8405505f3e37b0c6adde7c06
Jordan Meier and Rob King <jbirdmeier@gmail.com>
2016-01-28 15:25:54 -0800

    Add jQuery click event listenter and a for loop to make each individual letter clickable and show in the proper place in a word

--- js/scripts.js
+++ js/scripts.js
@@ -41,27 +41,39 @@ var newLetterArray = function (randomWordArray, index) {
 
 // //interface
 $(document).ready(function() {
+  var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
   var newWordArray = ["ocean", "beach", "dolphin", "starfish", "plankton", "waves", "sunshine", "coconut", "margarita"];
   var newWords = new Words(newWordArray);
   var wordIndex = newWordArray.length;
+
   $("#letsPlay").click(function() {
     $("#underscores").empty();
     wordIndex = wordIndex - 1;
     if(wordIndex > -1){
+      var turnCount = 6;
       var newWord = newRandomWord(newWords.randomWordArray, wordIndex);
       var letterArray = newLetterArray(newWords.randomWordArray, wordIndex);
       var newletterCount = letterArray.length;
       var columnWidth = Math.floor(12 / letterArray.length);
 
-      for (var j = 0; j < newletterCount; j++) {
-        // alert(columnWidth);
-        $("#underscores").append("<div class='col-md-" + columnWidth + " letterSpaces'></div>");
+      for (var i = 0; i < newletterCount; i++) {
+        $("#underscores").append("<div class='col-md-" + columnWidth + " letterSpaces'><span class='letters' id='letter" + i + "'>" + letterArray[i] + "</span></div>");
       };
+
+
+        $(".alphabetClick").click(function(){
+
+          var letterchoice = $(this).text();
+          for (var j = 0; j < newletterCount; j++){
+            if (letterchoice.toLowerCase() === letterArray[j].toLowerCase()){
+              $("#letter" + j).show();
+            };
+          };
+        });
+
     } else {
       alert("You've run out of words. Please Refresh the page to play again");
-    }
-
-
-
+    };
   });
 });
