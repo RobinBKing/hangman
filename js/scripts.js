d1820572964a3e4cbbbf32cbcf272694f4802c33
Jordan Meier and Rob King <jbirdmeier@gmail.com>
2016-01-28 16:40:30 -0800

    fix switch to first hangman img on new game

--- js/scripts.js
+++ js/scripts.js
@@ -42,29 +42,26 @@ var newLetterArray = function (randomWordArray, index) {
 // //interface
 $(document).ready(function() {
   $("#hangmanImg").append("<img src='img/hangman6.jpg'/>");
-
   var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
   var newWordArray = ["ocean", "beach", "dolphin", "starfish", "plankton", "waves", "sunshine", "coconut", "margarita"];
   var newWords = new Words(newWordArray);
   var wordIndex = newWordArray.length;
   $("#letsPlay").click(function() {
     $("#underscores").empty();
-    wordIndex = wordIndex - 1;
-    if(wordIndex > -1){
-      var turnCount = 6;
-      var newWord = newRandomWord(newWords.randomWordArray, wordIndex);
-      var letterArray = newLetterArray(newWords.randomWordArray, wordIndex);
-      var newletterCount = letterArray.length;
-      var columnWidth = Math.floor(12 / letterArray.length);
+      wordIndex = wordIndex - 1;
+      if(wordIndex > -1){
+        var turnCount = 6;
+        $("#hangmanImg").empty().append("<img src='img/hangman" + turnCount + ".jpg'/>");
+        var newWord = newRandomWord(newWords.randomWordArray, wordIndex);
+        var letterArray = newLetterArray(newWords.randomWordArray, wordIndex);
+        var newletterCount = letterArray.length;
+        var columnWidth = Math.floor(12 / letterArray.length);
 
-      for (var i = 0; i < newletterCount; i++) {
-        $("#underscores").append("<div class='col-md-" + columnWidth + " letterSpaces'><span class='letters' id='letter" + i + "'>" + letterArray[i] + "</span></div>");
-      };
-
+        for (var i = 0; i < newletterCount; i++) {
+          $("#underscores").append("<div class='col-md-" + columnWidth + " letterSpaces'><span class='letters' id='letter" + i + "'>" + letterArray[i] + "</span></div>");
+        };
 
         $(".alphabetClick").click(function(){
-
           var letterchoice = $(this).text();
           var searchCount = 0;
           for (var j = 0; j < newletterCount; j++){
@@ -78,17 +75,13 @@ $(document).ready(function() {
           if(searchCount >= letterArray.length){
             turnCount -= 1;
             $("#hangmanImg").empty().append("<img src='img/hangman" + turnCount + ".jpg'/>");
-
             if (turnCount === 0){
               alert("You lose!");
             };
-
           };
-
         });
-
-    } else {
-      alert("You've run out of words. Please Refresh the page to play again");
-    };
-  });
+      } else {
+        alert("You've run out of words. Please Refresh the page to play again");
+      };
+    });
 });
